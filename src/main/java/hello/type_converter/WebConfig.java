d4ebe1b76096395b227cb7332cd64a0c4eaafd06
Kwon seong jun <tjdwns1865@naver.com>
2022-07-21 23:23:01 +0900

    ConversionService 와 스프링에 적용해보기

---
+++ src/main/java/hello/type_converter/WebConfig.java
@@ -0,0 +1,21 @@
+package hello.type_converter;
+
+import hello.type_converter.converter.IntegerToStringConverter;
+import hello.type_converter.converter.IpPortToStringConverter;
+import hello.type_converter.converter.StringToIntegerConverter;
+import hello.type_converter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
